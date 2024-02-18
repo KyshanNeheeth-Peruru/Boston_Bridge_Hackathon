0bbd688c024e2578eac53d6915f63d3c62722368
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2024-02-18 04:09:31 -0500

    hackathon changes basic structure

--- bostonbridge/urls.py
+++ bostonbridge/urls.py
@@ -21,4 +21,25 @@ from bridge import views as bridge_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', bridge_views.home, name='home'),
+    path('login/', bridge_views.login, name='login'),
+    path('register/', bridge_views.register, name='register'),
+    path('forgot_password/', bridge_views.forgot_password, name='forgot_password'),
+    path('faq/', bridge_views.faq, name='faq'),
+    
+    path('urban_commuter/', bridge_views.urban_commuter, name='urban_commuter'),
+    path('city_Navigation/', bridge_views.city_Navigation, name='city_Navigation'),
+    path('local_community_engagement/', bridge_views.local_community_engagement, name='local_community_engagement'),
+    path('student_housing/', bridge_views.student_housing, name='student_housing'),
+    path('job_finder/', bridge_views.job_finder, name='job_finder'),
+    path('event_planning_tools/', bridge_views.event_planning_tools, name='event_planning_tools'),
+    path('campus_to_city_transition/', bridge_views.campus_to_city_transition, name='campus_to_city_transition'),
+    path('student_discounts/', bridge_views.student_discounts, name='student_discounts'),
+    path('emergency_response/', bridge_views.emergency_response, name='emergency_response'),
+    path('networking/', bridge_views.networking, name='networking'),
+    path('culture_exchange_platforms/', bridge_views.culture_exchange_platforms, name='culture_exchange_platforms'),
+    path('health_portal/', bridge_views.health_portal, name='health_portal'),
+    path('green_campus/', bridge_views.green_campus, name='green_campus'),
+    path('educational_partnerships/', bridge_views.educational_partnerships, name='educational_partnerships'),
+    path('ciy_exploration/', bridge_views.ciy_exploration, name='ciy_exploration'),
+
 ]
--- bridge/views.py
+++ bridge/views.py
@@ -4,3 +4,60 @@ from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def forgot_password(request):
+    return render(request, 'forgot_password.html')
+
+def faq(request):
+    return render(request, 'faq.html')
+
+def urban_commuter(request):
+    return render(request, 'urban_commuter.html')
+
+def city_Navigation(request):
+    return render(request, 'city_Navigation.html')
+
+def local_community_engagement(request):
+    return render(request, 'local_community_engagement.html')
+
+def student_housing(request):
+    return render(request, 'student_housing.html')
+
+def job_finder(request):
+    return render(request, 'job_finder.html')
+
+def event_planning_tools(request):
+    return render(request, 'event_planning_tools.html')
+
+def campus_to_city_transition(request):
+    return render(request, 'campus_to_city_transition.html')
+
+def student_discounts(request):
+    return render(request, 'student_discounts.html')
+
+def emergency_response(request):
+    return render(request, 'emergency_response.html')
+
+def networking(request):
+    return render(request, 'networking.html')
+
+def culture_exchange_platforms(request):
+    return render(request, 'culture_exchange_platforms.html')
+
+def health_portal(request):
+    return render(request, 'health_portal.html')
+
+def green_campus(request):
+    return render(request, 'green_campus.html')
+
+def educational_partnerships(request):
+    return render(request, 'educational_partnerships.html')
+
+def ciy_exploration(request):
+    return render(request, 'ciy_exploration.html')
