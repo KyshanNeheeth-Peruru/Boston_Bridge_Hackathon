8e3470ae191ddc32824bec506f27eb1853033437
KyshanNeheeth-Peruru <kishan.nehith@gmail.com>
2024-02-18 02:44:32 -0500

    finished setup

--- bostonbridge/urls.py
+++ bostonbridge/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from bridge import views as bridge_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', bridge_views.home, name='home'),
 ]
